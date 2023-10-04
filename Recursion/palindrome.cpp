532bc4a438a5fbb3564b456f0db387658e0b3233
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-05 00:16:33 +0530

    recursion added

--- Recursion/palindrome.cpp
+++ Recursion/palindrome.cpp
@@ -1,32 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
- bool isPalindrome(string &s , int i, int j){
+//  bool isPalindrome(string &s , int i, int j){
 
-    //base case
-    if(i>j){
+//     //base case
+//     if(i>j){
+//         return true;
+//     }
+
+//     if(s[i]!=s[j]){
+//         return false;
+//     }
+//     else{
+//        return isPalindrome(s,i+1,j-1);
+//     }
+//  }
+
+bool checkPalindrome(string &s, int i)
+{
+
+    // base case
+    if (i >= s.size() / 2)
+    {
+
         return true;
     }
 
-    if(s[i]!=s[j]){
+    if (s[i] != s[s.size() - i - 1])
+    {
         return false;
     }
-    else{
-       return isPalindrome(s,i+1,j-1);
-    } 
- }
 
-int main(){
+    return checkPalindrome(s, i + 1);
+}
 
-    string s ="1211";
+int main()
+{
 
-   if( isPalindrome(s,0,s.length()-1)){
-    cout<<"palindrome"<<endl;
-   }
-   else{
-    cout<<"not palindrom"<<endl;
-   }
+    string s = "1212541";
 
+    if (checkPalindrome(s, 0))
+    {
+        cout << "palindrome" << endl;
+    }
+    else
+    {
+        cout << "not palindrom" << endl;
+    }
 
     return 0;
 }

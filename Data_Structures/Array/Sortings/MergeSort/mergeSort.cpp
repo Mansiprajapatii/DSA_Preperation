10d8bff7ba9419eab9bf9dca6044bef4bc869a0f
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-06 00:59:53 +0530

    sorting II  done

--- Data_Structures/Array/Sortings/MergeSort/mergeSort.cpp
+++ Data_Structures/Array/Sortings/MergeSort/mergeSort.cpp
@@ -1,65 +1,133 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void merge(int arr[], int l, int mid, int h){
-    int s=h;
-    int b[s++];
-    int k=l;
-    int i=l;
-    int j=mid+1;
+// void merge(int arr[], int l, int mid, int h){
+//     int s=h;
+//     int b[s++];
+//     int k=l;
+//     int i=l;
+//     int j=mid+1;
 
-   while(i<=mid && j<=h){
-            if(arr[i]<=arr[j]){
-                b[k]=arr[i];
-                k++;
-                i++;
-            }
-            else{
-                b[k]=arr[j];
-                k++;
-                j++;
-            }
+//    while(i<=mid && j<=h){
+//             if(arr[i]<=arr[j]){
+//                 b[k]=arr[i];
+//                 k++;
+//                 i++;
+//             }
+//             else{
+//                 b[k]=arr[j];
+//                 k++;
+//                 j++;
+//             }
+//         }
+
+//     while(i<=mid){
+//         b[k]=arr[i];
+//         k++;
+//         i++;
+//     }
+
+//     while(j<=h){
+//         b[k]=arr[j];
+//         k++;
+//         j++;
+//     }
+
+//     for(int p=l; p<=h; p++){
+//         arr[p]=b[p];
+//     }
+
+// }
+
+// void mergeSot( int arr[], int l, int h){
+//     if(l==h){
+//         return;
+//     }
+
+//     int mid = (l+h)/2;
+
+//     mergeSot(arr,l,mid);
+//     mergeSot(arr,mid+1,h);
+//     merge(arr,l,mid,h);
+
+// }
+
+void merge(int arr[], int low, int mid, int high)
+{
+
+    int size = high + 1;
+    int temp[size];
+    int left = low;
+    int right = mid + 1;
+    int help = low;
+
+    while (left <= mid && right <= high)
+    {
+
+        if (arr[left] < arr[right])
+        {
+            temp[help] = arr[left];
+            left++;
+            help++;
         }
-    
-    while(i<=mid){
-        b[k]=arr[i];
-        k++;
-        i++;
+        else
+        {
+            temp[help] = arr[right];
+            right++;
+            help++;
+        }
     }
 
-    while(j<=h){
-        b[k]=arr[j];
-        k++;
-        j++;
+    while (left <= mid)
+    {
+        temp[help] = arr[left];
+        help++;
+        left++;
     }
 
-    for(int p=l; p<=h; p++){
-        arr[p]=b[p];
+    while (right <= high)
+    {
+        temp[help] = arr[right];
+        help++;
+        right++;
     }
 
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i];
+    }
 }
 
-void mergeSot( int arr[], int l, int h){
-    if(l==h){
+void mergeSort(int arr[], int low, int high)
+{
+
+    // base case
+    if (low >= high)
+    {
         return;
     }
-      
-    int mid = (l+h)/2;
 
-    mergeSot(arr,l,mid);
-    mergeSot(arr,mid+1,h);
-    merge(arr,l,mid,h);
-    
+    int mid = (low + high) / 2;
+    // left side of the array to be divided
+    mergeSort(arr, low, mid);
+
+    // right side of the array
+    mergeSort(arr, mid + 1, high);
+
+    // now merge the single sorted arrays
+    merge(arr, low, mid, high);
 }
 
-int main(){
+int main()
+{
 
-    int arr[9] = {50,40,20,10,50,10,70,60,80};
+    int arr[9] = {50, 40, 20, 10, 50, 10, 70, 60, 80};
 
-    mergeSot(arr,0,8);
+    mergeSort(arr, 0, 8);
 
-   for(int i=0; i<=8; i++){
-    cout<<arr[i]<<" "; 
-  }
+    for (int i = 0; i <= 8; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }

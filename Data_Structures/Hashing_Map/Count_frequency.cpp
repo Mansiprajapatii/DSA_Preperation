92789b3aa40ff3ff50553f5b5952cf362e74c01b
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-06 12:22:02 +0530

    hashing added

--- Data_Structures/Hashing_Map/Count_frequency.cpp
+++ Data_Structures/Hashing_Map/Count_frequency.cpp
@@ -4,17 +4,43 @@ using namespace std;
 
 int main()
 {
+    // Number Hashing
+    //  int n = 12;
 
-    int n = 12;
+    // int arr[12] = {1, 3, 2, 1, 3};
 
-    int arr[12] = {1, 3, 2, 1, 3};
+    // int hash[13] = {0};
 
-    int hash[13] = {0};
+    // for (int i = 0; i < 12; i++)
+    // {
+    //     hash[arr[i] - 1] += 1;
+    // }
 
-    for (int i = 0; i < 12; i++)
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << hash[i] << " ";
+    // }
+
+    // Character Hashing
+    // use n=26 and ch-'a' as string only has lowercase letters
+    int n = 26;
+    string s = "abcdabehf";
+
+    int hash[n] = {0};
+
+    for (int i = 0; i < s.size(); i++)
     {
-        hash[arr[i] - 1] += 1;
-    } 
+        hash[s[i] - 'a']++;
+    }
+
+    // if it does not mention the lowercase letters use the size 256 size array and simply do hash[s[i]]++
+    // int n = 256;
+    // string s = "abcGSDIFAMEQAOEWdabehf";
+
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     hash[s[i]]++;
+    // }
 
     for (int i = 0; i < n; i++)
     {

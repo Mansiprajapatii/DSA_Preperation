dba8a3fee9e411402141862de95f8a2c4542bb47
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-08 17:01:45 +0530

    array problem

--- Data_Structures/Array/Array_practice_ques/remove_duplicate.cpp
+++ Data_Structures/Array/Array_practice_ques/remove_duplicate.cpp
@@ -2,8 +2,9 @@
 #include <algorithm>
 using namespace std;
 
-
-
+// using brute force 
+// TC - O{N}
+// SC - O{1}
 int removeDuplicates(vector<int> &arr, int n) {
 	int size = 0;
 
@@ -18,7 +19,54 @@ int removeDuplicates(vector<int> &arr, int n) {
 	return size;
 }
 
-int main(){
+// using sets 
+//time complexity = O{N log N}+ N
+// space complexity = O{N}
+
+int remove_duplicate(vector<int> arr, int n)
+{
+    set<int> s;
+
+    for (int i = 0; i < n; i++)
+    {
+        s.insert(arr[i]);
+    }
+
+    int index = 0;
+
+    for (auto it : s)
+    {
+        arr[index] = it;
+        index++;
+    }
+
+    return index;
+}
+
+// using twp pointer approach 
+// TC- O{N}
+// SC - O[1]
+int remove__duplicate(vector<int> arr, int n){
+    int i=0;
+
+    for(int j=1; j<n; j++){
+        if(!arr[j] == arr[i]){
+            arr[i+1] = arr[j];
+            i++;
+        }
+    }
+
+    return i+1;
+}
+
+
+
+int main()
+{
+    int n = 7;
+    vector<int> arr = {2, 3, 1, 6, 3, 6, 2};
+
+    cout << remove_duplicate(arr, n);
 
     return 0;
 }

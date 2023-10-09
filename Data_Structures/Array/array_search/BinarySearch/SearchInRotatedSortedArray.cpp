3e16c25c1b1c84494bed4b87406c6461d1f2b7c1
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-09 21:55:07 +0530

    binary search problem

--- Data_Structures/Array/array_search/BinarySearch/SearchInRotatedSortedArray.cpp
+++ Data_Structures/Array/array_search/BinarySearch/SearchInRotatedSortedArray.cpp
@@ -1,58 +1,125 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int Pivot_ele(int arr[], int a){
-    int s=0;
-    int e = a-1;
+// One solution
 
-    while(s<e){
-        int mid = s+ (e-s)/2;
+int Pivot_ele(vector<int> &arr, int n)
+{
 
-        //if array is not rotated thn 
-        if(arr[e]>arr[0]){
+    int s = 0;
+    int e = n - 1;
+
+    while (s < e)
+    {
+        int mid = s + (e - s) / 2;
+
+        if (arr[e] > arr[0])
+        {
             return 0;
         }
-        if(arr[mid]>=arr[0]){
-            s= mid+1;
+
+        if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
         }
-        else{
-            e=mid;
+        else
+        {
+            e = mid;
         }
     }
     return s;
 }
 
-int binarySearch(int arr[], int s, int e, int k){
-    while(s<=e){
-        int mid= s+(e-s)/2;
+int binarySearch(vector<int> &arr, int s, int e, int k)
+{
 
-        if(arr[mid]==k){
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+
+        if (arr[mid] == k)
+        {
             return mid;
         }
-        if(arr[mid]<k){
-            s=mid+1;
+        if (arr[mid] < k)
+        {
+            s = mid + 1;
         }
-        else{
-            e=mid-1;
+        else
+        {
+            e = mid - 1;
         }
     }
     return -1;
 }
 
+int findPosition(vector<int> &arr, int n, int k)
+{
+    int pivot = Pivot_ele(arr, n);
 
-int main(){
-    int arr[7] ={2,4 ,5, 6, 8, 9, 1};
-    int k=20;
-
-    int pivot = Pivot_ele(arr,7);
-
-    cout<<"pivot"<<pivot<<endl;
-
-    if(k>=arr[pivot] && k<= arr[6]){
-        cout<<" "<< binarySearch(arr,pivot,6,k)<<endl;
+    if (k >= arr[pivot] && k <= arr[n - 1])
+    {
+        return binarySearch(arr, pivot, n - 1, k);
     }
-    else{
-         cout<<" "<< binarySearch(arr,0,pivot-1, k)<<endl;
+    else
+    {
+        return binarySearch(arr, 0, pivot - 1, k);
     }
-   
+}
+
+// another soution
+int search(vector<int> &arr, int n, int k)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == k)
+        {
+            return mid;
+        }
+
+        // else identify the sorted part
+        // left part checking
+        if (arr[low] <= arr[mid])
+        {
+            // check element lie int this range
+            if (arr[low] <= k && k <= arr[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        // check for right part
+        else
+        {
+
+            if (arr[mid] <= k && k <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[7] = {2, 4, 5, 6, 8, 9, 1};
+    int k = 20;
+
+    return 0;
 }

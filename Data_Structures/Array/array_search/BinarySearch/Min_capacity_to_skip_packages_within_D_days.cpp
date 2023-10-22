b12f057d70a7d0097398a0c72e49b6dd4cb3f142
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-22 21:56:40 +0530

    find first and last occurrence of element in array solution added

--- Data_Structures/Array/array_search/BinarySearch/Min_capacity_to_skip_packages_within_D_days.cpp
+++ Data_Structures/Array/array_search/BinarySearch/Min_capacity_to_skip_packages_within_D_days.cpp
@@ -1,22 +1,25 @@
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-//TC- O[N * log(sum-max)]
-// SC - O[1]
+// TC- O[N * log(sum-max)]
+//  SC - O[1]
 
-
-int daysReq(vector<int> &weights , int cap){
-    int day =1; 
-    int load= 0;
+int daysReq(vector<int> &weights, int cap)
+{
+    int day = 1;
+    int load = 0;
     int n = weights.size();
 
-    for(int i=0; i<n; i++){
-        if(load+weights[i] > cap){
+    for (int i = 0; i < n; i++)
+    {
+        if (load + weights[i] > cap)
+        {
             day++;
             load = weights[i];
         }
-        else{
+        else
+        {
             load += weights[i];
         }
     }
@@ -27,7 +30,7 @@ int daysReq(vector<int> &weights , int cap){
 int leastWeightCapacity(vector<int> &weights, int d)
 {
     int maxi = INT_MIN;
-    int sum =0;
+    int sum = 0;
     int n = weights.size();
 
     // for(int i=0; i<n; i++){
@@ -36,19 +39,22 @@ int leastWeightCapacity(vector<int> &weights, int d)
     // }
 
     maxi = *max_element(weights.begin(), weights.end());
-    sum = accumulate(weights.begin(), weights.end() , 0);
+    sum = accumulate(weights.begin(), weights.end(), 0);
 
     int low = maxi;
-    int high =  sum;
+    int high = sum;
 
-    while(low<=high){
-        int mid = low + (high-low) /2;
-        int minDay = daysReq(weights,mid);
-        if(minDay <= d){
-            high = mid-1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int minDay = daysReq(weights, mid);
+        if (minDay <= d)
+        {
+            high = mid - 1;
         }
-        else{
-            low= mid+1;
+        else
+        {
+            low = mid + 1;
         }
     }
 

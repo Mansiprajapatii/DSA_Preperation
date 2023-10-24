1448216aebaa5f78c0f45126d4d754c5eebd88b3
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-24 23:12:19 +0530

    Sliding Window ques added

---
+++ Sliding_window/Longest_substring_without_repeating_char.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ int lengthOfLongestSubstring(string s) {
+        vector<int> mapp(256,-1);
+        int left =0, right=0;
+        int len=0;
+        int n= s.size();
+
+        while(right < n){
+            if(mapp[s[right]] != -1) {
+                left = max(left, mapp[s[right]]+1);
+            }
+            mapp[s[right]] = right;
+
+            len = max(len, right-left+1);
+
+            right++;
+           
+        }
+        return len;
+    }

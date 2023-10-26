ab6abe4fcafa4caf056106a9c7dab06ef0fa49b3
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-10-26 23:07:21 +0530

    array ques added

---
+++ Data_Structures/Array/Array_practice_ques/Pascal's_triangle.cpp
@@ -0,0 +1,32 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// solved using dp
+
+vector<int> generateRow(int n)
+{
+
+    vector<int> ansRow;
+    ansRow.push_back(1);
+    if (n == 1)
+        return ansRow;
+    long long int ans = 1;
+    for (int i = 1; i < n; i++)
+    {
+        ans = ans * (n - i);
+        ans = ans / i;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+vector<vector<int>> pascalTriangle(int N)
+{
+    vector<vector<int>> ans;
+    for (int i = 1; i <= N; i++)
+    {
+        ans.push_back(generateRow(i));
+    }
+    return ans;
+}

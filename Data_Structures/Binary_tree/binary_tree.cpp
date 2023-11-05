2ac52f1b1bded3bf011cc0d633048e57d08ba2cc
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-11-05 21:08:12 +0530

    Binary Tree DS added

---
+++ Data_Structures/Binary_tree/binary_tree.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node
+{
+
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main()
+{
+
+    node *root = new node(1);
+
+    root->left = new node(2);
+    root->right = new node(3);
+
+    root->left->right = new node(4);
+
+    return 0;
+}

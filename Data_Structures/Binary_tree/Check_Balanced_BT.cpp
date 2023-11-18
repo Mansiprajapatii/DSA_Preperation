04f05cfad74f2eaabb02b8b64b10ee45bb2eeed8
Mansiprajapatii <82796129+Mansiprajapatii@users.noreply.github.com>
2023-11-18 21:04:09 +0530

    BST

--- Data_Structures/Binary_tree/Check_Balanced_BT.cpp
+++ Data_Structures/Binary_tree/Check_Balanced_BT.cpp
@@ -10,3 +10,30 @@ struct TreeNode {
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return dfs(root) != -1 ;
+    }
+private:
+    
+    int dfs(TreeNode* root) {
+    
+     if(root == NULL) return 0;
+
+     int lh = dfs(root->left);
+
+     if(lh == -1 ) return -1;
+
+     int rh = dfs(root->right);
+
+     if(rh == -1) return -1;
+
+     if(abs(lh-rh) > 1) return -1;
+
+     return 1+max(lh,rh);
+    }
+
+};
